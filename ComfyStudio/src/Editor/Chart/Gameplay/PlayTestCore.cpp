f1199ba118539e2179fe48a04cb7ce4a2b9dcc7e
hiki8man <gkw189@qq.com>
2025-06-23 19:19:50 +0800

    fix playtest success sound doesn't work

--- ComfyStudio/src/Editor/Chart/Gameplay/PlayTestCore.cpp
+++ ComfyStudio/src/Editor/Chart/Gameplay/PlayTestCore.cpp
@@ -1249,8 +1249,9 @@ namespace Comfy::Studio::Editor
 					const auto fineThreshold = IsSlideButtonType(nextTargetToHit->Type) ? HitThreshold::FineSlide : HitThreshold::Fine;
 
 					bool successfulHit = false;
-
-					if (matchingType && IsSlideButtonType(nextTargetToHit->Type))
+					if (nextTargetToHit->Flags.IsChance)
+						sharedContext.ButtonSoundController->PlayChanceSound();
+					else if (matchingType && IsSlideButtonType(nextTargetToHit->Type))
 					{
 						sharedContext.ButtonSoundController->PlaySlideSound();
 						lastSlideActionStopwatch.Restart();
