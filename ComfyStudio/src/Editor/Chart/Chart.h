dbcaacfc7b3ec47167046a6806194a3444c6afee
Wei-Cheng Yeh (IID) <iid@ccns.ncku.edu.tw>
2025-07-04 17:39:15 +0800

    [X] Add change success note support -> [O] QOL

--- ComfyStudio/src/Editor/Chart/Chart.h
+++ ComfyStudio/src/Editor/Chart/Chart.h
@@ -64,7 +64,7 @@ namespace Comfy::Studio::Editor
 	public:
 		// NOTE: In case there is no audio file to take as a reference
 		static constexpr auto FallbackDuration = TimeSpan::FromMinutes(1.5);
-		static constexpr auto FallbackSongTitle = std::string_view { u8"ƒ_ƒ~[" };
+		static constexpr auto FallbackSongTitle = std::string_view { u8"×VÃæ×÷³É" };
 
 	public:
 		// NOTE: An empty path means the chart hasn't yet been saved to nor has been loaded from disk
